abf7f260178e69d71e8e39e73c34db4dbbbe864e
Zoltan Biber <zoltan.biber@gmail.com>
2014-04-16 21:11:58 +0100

    choosing ending line and ending station added

--- tfl.rb
+++ tfl.rb
@@ -40,3 +40,44 @@ puts tube_lines[starting_line]
 puts "PLEASE CHOOSE YOUR STARTING STATION\!"
 print prompt
 starting_station = gets.strip.chomp.downcase
+puts "YOUR STARTING STATION IS #{starting_station.upcase}"
+
+
+# CHOOSING ENDING LINE AND ENDING STATION
+puts "PLEASE CHOOSE YOUR ENDING LINE"
+puts "PLEASE TYPE VICTORIA, BAKERLOO OR CENTRAL AND PRESS ENTER"
+print prompt
+ending_line = gets.strip.chomp.downcase
+
+while true 
+	case ending_line
+	  when "victoria"
+	  	puts "YOUR ENDING LINE IS #{ending_line.upcase}"
+	  when "bakerloo"
+	  	puts "YOUR ENDING LINE IS #{ending_line.upcase}"
+	  when "central"
+		puts "YOUR ENDING LINE IS #{ending_line.upcase}"  	
+	  else
+		puts "I DIDN'T QUITE GET THAT, YOU MAY HAVE MISTYPED IT, PLEASE CHOOSE AGAIN\!"
+		print prompt
+		ending_line = gets.strip.chomp.downcase  
+		redo	
+	end
+	break
+end
+
+puts "THE STATIONS ON THE #{ending_line.upcase} LINE ARE THE FOLLOWING: "
+puts tube_lines[ending_line]
+puts "PLEASE CHOOSE YOUR ENDING STATION\!"
+print prompt
+ending_station = gets.strip.chomp.downcase
+puts "YOUR ENDING STATION IS #{ending_station.upcase}"
+
+
+binding.pry
+
+# [2] pry(main)> tube_lines[starting_line].index("victoria")
+# => nil
+# [3] pry(main)> tube_lines[starting_line].index("Victoria")
+# => 5
+# [4] pry(main)>
