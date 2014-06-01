9473fffbda460b02dba97853f644141fd22ddf65
Zoltan Biber <zoltan.biber@gmail.com>
2014-06-01 11:19:43 +0100

    number of stops between starting station and end station are printed on screen /// change at oxford circus taken into account

--- tfl.rb
+++ tfl.rb
@@ -5,15 +5,15 @@ bakerloo = ["Elephant & Castle", "Lambeth North", "Waterloo", "Embankment", "Cha
 central = ["Notting Hill Gate", "Queensway", "Lancaster Gate", "Marble Arch", "Bond Street", "Oxford Circus", "Tottenham Court Road", "Holborn", "Chancery Lane"]
 prompt = ">>>  "
 
+puts ""
 puts "WELCOME TO THE LONDON JOURNEY PLANNER"
 puts "THE FOLLOWING 3 LINES ARE RUNNING TODAY: VICTORIA / BAKERLOO / CENTRAL"
 puts "PLEASE CHOOSE YOUR STARTING LINE"
 puts "PLEASE TYPE VICTORIA, BAKERLOO OR CENTRAL AND PRESS ENTER"
 print prompt
 
-# check if the user has put in the right string
-# in case of a typo alert the user and ask him to choose starting line again
 
+# CHOOSING STARTING LINE AND STARTING STATION
 starting_line = gets.strip.chomp.downcase
 
 while true 
@@ -25,6 +25,7 @@ while true
 	  when "central"
 		puts "YOUR STARTING LINE IS #{starting_line.upcase}"  	
 	  else
+		# in case of a typo alert the user and ask him to choose starting line again
 		puts "I DIDN'T QUITE GET THAT, YOU MAY HAVE MISTYPED IT, PLEASE CHOOSE AGAIN\!"
 		print prompt
 		starting_line = gets.strip.chomp.downcase  
@@ -38,8 +39,10 @@ tube_lines = {"victoria" => victoria, "bakerloo" => bakerloo, "central" => centr
 puts "THE STATIONS ON THE #{starting_line.upcase} LINE ARE THE FOLLOWING: "
 puts tube_lines[starting_line]
 puts "PLEASE CHOOSE YOUR STARTING STATION\!"
+puts "PLEASE TYPE THE NAME OF THE STATION EXACTLY AS IT IS LISTED"
 print prompt
 starting_station = gets.strip.chomp.downcase
+
 puts "YOUR STARTING STATION IS #{starting_station.upcase}"
 
 
@@ -49,18 +52,18 @@ puts "PLEASE TYPE VICTORIA, BAKERLOO OR CENTRAL AND PRESS ENTER"
 print prompt
 ending_line = gets.strip.chomp.downcase
 
-while true 
+while true
 	case ending_line
 	  when "victoria"
 	  	puts "YOUR ENDING LINE IS #{ending_line.upcase}"
 	  when "bakerloo"
 	  	puts "YOUR ENDING LINE IS #{ending_line.upcase}"
 	  when "central"
-		puts "YOUR ENDING LINE IS #{ending_line.upcase}"  	
+			puts "YOUR ENDING LINE IS #{ending_line.upcase}"  	
 	  else
-		puts "I DIDN'T QUITE GET THAT, YOU MAY HAVE MISTYPED IT, PLEASE CHOOSE AGAIN\!"
-		print prompt
-		ending_line = gets.strip.chomp.downcase  
+			puts "I DIDN'T QUITE GET THAT, YOU MAY HAVE MISTYPED IT, PLEASE CHOOSE AGAIN\!"
+			print prompt
+			ending_line = gets.strip.chomp.downcase  
 		redo	
 	end
 	break
@@ -69,15 +72,29 @@ end
 puts "THE STATIONS ON THE #{ending_line.upcase} LINE ARE THE FOLLOWING: "
 puts tube_lines[ending_line]
 puts "PLEASE CHOOSE YOUR ENDING STATION\!"
+puts "PLEASE TYPE THE NAME OF THE STATION EXACTLY AS IT IS LISTED"
 print prompt
 ending_station = gets.strip.chomp.downcase
 puts "YOUR ENDING STATION IS #{ending_station.upcase}"
 
+# DEFINING THE INDEXES OF THE STARTING AND ENDING STATIONS WITHIN THEIR OWN LINES
+starting_station_capitalized = starting_station.split.map(&:capitalize).join(' ')
+start_index = tube_lines[starting_line].index(starting_station_capitalized)
 
-binding.pry
+ending_station_capitalized = ending_station.split.map(&:capitalize).join(' ')
+end_index = tube_lines[ending_line].index(ending_station_capitalized)
 
-# [2] pry(main)> tube_lines[starting_line].index("victoria")
-# => nil
-# [3] pry(main)> tube_lines[starting_line].index("Victoria")
-# => 5
-# [4] pry(main)>
+# IF STARTING LINE AND ENDING LINE ARE THE SAME PRINT OUT THE NUMBER OF STOPS AND STATION NAMES
+# ELSE WE NEED TO CHANGE AT OXFORD CIRCUS...
+if starting_line == ending_line
+	distance = (end_index - start_index).abs
+	puts "YOU HAVE #{distance} STOPS TO GO"
+else
+	puts "* * * NOTICE * * *: YOU NEED TO CHANGE LINES AT OXFORD CIRCUS STATION!"
+	oxford_circus_index = tube_lines[starting_line].index("Oxford Circus")
+	distance_to_oxford_circus = (start_index - oxford_circus_index).abs 
+	puts "YOU NEED TO GO #{distance_to_oxford_circus} STOPS TO OXFORD CIRCUS"
+	oxford_circus_index = tube_lines[ending_line].index("Oxford Circus")
+	distance_from_oxford_circus = (end_index - oxford_circus_index).abs
+	puts "AFTER CHANGING LINES YOU HAVE #{distance_from_oxford_circus} MORE STOPS TO GO"
+end
